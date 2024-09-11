428eab919fbc15cc88daf661e3ec97b79983225d
Heather-Yaple <heatherlyaple@gmail.com>
2024-09-11 17:59:24 -0400

    "final Changes"

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,9 +1,18 @@
 
 function bubbleSort(arr) {
+  let n = arr.length;
   let swapped;
+
+
   do {
     swapped = false;
+
+
+    //Traverse through all elements in the array
+
     for (let i = 0; i < arr.length-1; i++) {
+      
+      // Compare adjacent elements and swap if needed
       if(arr[i] > arr[i + 1]) {
         let temp = arr[i];
         arr[i] = arr[i + 1];
@@ -11,11 +20,8 @@ function bubbleSort(arr) {
         swapped = true;
       }
     }
-  } while (swapped);
 
-arr = [8, 20, -2, 4, -6]
-bubbleSort(arr)
-console.log(arr)
+
 
   
 
@@ -28,7 +34,12 @@ console.log(arr)
     // If you get to the end of the array and no swaps have occurred, return
 
     // Otherwise, repeat from the beginning
+    n--;
+  } while (swapped);
 
+  //arr = [8, 20, -2, 4, -6]
+  //bubbleSort(arr)
+  //console.log(arr)
 }
 
 module.exports = bubbleSort;
